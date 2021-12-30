ff93e306130ec8e07e5b9134c9c83733f8157e3f
kaustubh87 <kaustubh87@gmail.com>
2021-12-30 09:47:33 -0500

    Updated with arrow functions instead of plain js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       title: "Toilet Paper",
@@ -27,6 +27,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
