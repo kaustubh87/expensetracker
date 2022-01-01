9abde8d169b70ca3fbe96a6d11dba645b22d8ff8
kaustubh87 <kaustubh87@gmail.com>
2022-01-01 17:59:41 -0500

    Added feature to populate lists dynamically using map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,7 +17,14 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -36,7 +43,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
